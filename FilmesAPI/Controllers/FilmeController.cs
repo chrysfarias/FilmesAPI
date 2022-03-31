84d2a157a5435c691aa08201445f2f914b2f13e1
chrystian farias <chrystianfarias@hotmail.com>
2022-03-31 17:57:40 -0400

    first commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class FilmeController : ControllerBase
+    {
+        private static list<Filme> filmes = new List<Filme>();
+        public void AdicionaFilme(Filme filme)
+        {
+            
+        }
+    }
+}
+
