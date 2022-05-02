9d41a1c4157647b641078d24f1ed1ef5c093f6f0
chrystian farias <chrystianfarias@hotmail.com>
2022-05-02 08:36:32 -0400

    Retornando status code e Location

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 using FilmesAPI.Models;
 
 
-
-
-
 namespace FilmesAPI.Controllers
 {
     [ApiController]
@@ -21,21 +18,22 @@ namespace FilmesAPI.Controllers
 
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperaFilme()
+        public IActionResult RecuperaFilmes()
         {
             return Ok(filmes);
         }
 
         [HttpGet("{id}")]
 
-        public IActionResult RecuperaFilmePorId(int id)
+        public IActionResult RecuperaFilmesPorId(int id)
         {
           Filme filme = filmes.FirstOrDefault(filme => filme.Id == id );
             if(filme != null)
