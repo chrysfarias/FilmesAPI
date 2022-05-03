94dc7800c29a54bc8ba4468c0fa313ab5a247407
chrystian farias <chrystianfarias@hotmail.com>
2022-05-03 07:25:09 -0400

    Migration

---
+++ FilmesAPI/Migrations/20220503111748_CriandoTabelaDeFilme.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace FilmesAPI.Migrations
+{
+    public partial class CriandoTabelaDeFilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Duracao = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Diretor = table.Column<string>(type: "text", nullable: false),
+                    Genero = table.Column<string>(type: "varchar(35)", maxLength: 35, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
